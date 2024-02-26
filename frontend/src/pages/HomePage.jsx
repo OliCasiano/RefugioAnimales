aa5958b202e06242d47aa17c3c244f668fbf40b8
San-QP <s.rebollar.g@gmail.com>
2024-02-26 09:48:27 +0100

    Estilo de las cards

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,27 +1,27 @@
 
 import { Header } from '../components/header/Header'
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import { Card } from '../components/Card/Card';
 
 export const HomePage = () => {
-  const [cards, setCards] = useEstate("")
+  // const [cards, setCards] = useState("")
   
-  const URL = "http://localhost:8080/announcement";
+  // const URL = "http://localhost:8080/announcement";
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(URL);
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        setCards(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-  })()
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch(URL);
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       const data = await response.json();
+  //       setCards(data);
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
+  // })()
     
 
   return (
